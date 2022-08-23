8e2dee666ec7779b39117801887fca75c60c539b
jromatowski <joshuaromatowski@gmail.com>
2022-08-23 16:16:48 -0700

    v.01 code done, host next

--- flute_app_backend/flute_app_backend/settings.py
+++ flute_app_backend/flute_app_backend/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'practice_tracker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'practice_tracker'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'flute_app_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'practice_sessions',
     }
 }
 
