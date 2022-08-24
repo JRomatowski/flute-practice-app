bb417773b2f3daf46372ccfb44efb5ddb74e88ea
jromatowski <joshuaromatowski@gmail.com>
2022-08-24 10:24:05 -0700

    WTF

--- flute_app_frontend/src/components/Home.js
+++ flute_app_frontend/src/components/Home.js
@@ -5,7 +5,7 @@ function Home() {
         <>  
             <div id='home-screen-container'> 
                 <section id='home-screen-text'>
-                    <h2 id='home-title-text'>Flutemajig</h2>
+                    <h2 id='home-title-text'>Flute Practice App</h2>
                     <div id='blurb'>
                         <p>Enter in your information below:</p>
                     </div>
