96470ff3004ad6ab1983acf44eab211c80f16673
jromatowski <joshuaromatowski@gmail.com>
2022-08-25 16:53:51 -0700

    delete works

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,7 +1,22 @@
 import '../App.css'
 import { useNavigate } from 'react-router-dom'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Review() {
+
+    const [sessionHistory, setSessionHistory] = useState([])
+
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/history')
+            .then(res => {
+                let data = res.data.practice_sessions
+                // Not sure why thing needs an arrow => to thing again.  This works though.
+                let reversedData = data.map(thing => thing).reverse();
+                setSessionHistory(reversedData[0])
+            })
+    },[])
 
     const navigate = useNavigate();
 
@@ -10,16 +25,14 @@ function Review() {
     }
 
     const sessionClick = function() {
+        let idToDelete = sessionHistory.id
+        console.log(idToDelete)
+        axios.delete(`http://127.0.0.1:8000/history/${idToDelete}`, idToDelete)
         navigate('/Session')
     }
 
     const deleteClick = function() {
         navigate('/Review')
-        // if form empty {
-        //     navigate('/Initiate')
-        // } else {
-        //     return
-        // }
     } 
 
     return(
@@ -27,11 +40,11 @@ function Review() {
             <h3>Review Component</h3>
             <div className='review-container'>
                 <h4>Summary of Session</h4>
-                <p>Length:</p>
-                <button onClick={initiateClick}>Submit/Home</button><br></br><br></br>
+                <p>Length: {sessionHistory.length} minutes</p>
+                <button onClick={initiateClick}>Submit/Return Home</button><br></br><br></br>
                 <button>Share via Email</button><br></br><br></br>
                 <button onClick={sessionClick}>Edit</button><br></br><br></br>
-                <button onClick={deleteClick}>Delete</button>
+                <button onClick={deleteClick}>Delete/Return Home</button>
             </div>
         </>
     )
--- src/components/Session.js
+++ src/components/Session.js
@@ -15,21 +15,14 @@ function Session() {
     }
 
     const handleChange = function(event) {
-        setFormState({
-            length: event.target.value
-        })
-        console.log(formState)
+        setFormState({length: event.target.value})
+        // console.log(formState)
     }
 
     const reviewClick = function(event) {
         event.preventDefault()
-        let newSession = {"practice_sessions": [formState]            
-        }
-
-        console.log(newSession)
-
+        let newSession = {"practice_sessions": [formState]}
         axios.post('http://127.0.0.1:8000/history/', newSession)
-
         navigate('/Review')
     }
 
@@ -41,11 +34,9 @@ function Session() {
         navigate('/')
     }
 
-
     return(
         <>  
             <h3>Session Component</h3>
-
             <form onSubmit={reviewClick}>
                 <div>Length of session?</div><br></br>
                 <input type='number' onChange={handleChange}/><br></br><br></br>
