e050acfb94598b87774e5266cabdc5784f45467c
jromatowski <joshuaromatowski@gmail.com>
2022-10-17 12:44:03 -0700

    fixed

--- src/components/HistoryStats/HistoryStats.js
+++ src/components/HistoryStats/HistoryStats.js
@@ -2,7 +2,6 @@ import './HistoryStats.css'
 import axios from 'axios'
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom'
-import Chart1 from '../Chart1';
 
 function HistoryStats() {
 
